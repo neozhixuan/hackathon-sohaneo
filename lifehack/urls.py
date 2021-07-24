b90086310164a20bbd7f254c1665377e91a6e785
Neo Zhi Xuan <neozhixuan@gmail.com>
2021-07-24 12:36:40 +0800

    .

--- lifehack/urls.py
+++ lifehack/urls.py
@@ -23,7 +23,7 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include("Home.urls")),
+    path('', include("project.urls")),
     url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
     url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
